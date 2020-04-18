8ea3f3ef7efeafc2d660f9aeab27de5330fdfcac
YamadaTakahito <d_p.t.a.d.fighter@icloud.com>
2020-04-18 15:09:32 +0900

    add Chap1.java

---
+++ src/main/java/DesignPatternMultiThread/chap1/Chap1.java
@@ -0,0 +1,9 @@
+package DesignPatternMultiThread.chap1;
+
+public class Chap1 {
+    public static void main() {
+        for (int i = 0; i < 10000; i++){
+            System.out.println("Good!");
+        }
+    }
+}
